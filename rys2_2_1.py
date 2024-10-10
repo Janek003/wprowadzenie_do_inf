e4afc2cfe9a7b4c5dd8e48f1e8081b937897e392
Jan Porębski <120678651+Janek003@users.noreply.github.com>
2024-10-10 17:41:35 +0200

    Add files via upload

---
+++ rys2_2_1.py
@@ -0,0 +1,22 @@
+from random import *
+from math import *
+
+losowa = randint(1,20)
+gracz1 = int(input('(gracz1) Zgadnij liczbe od 1 do 20: '))
+gracz2 = int(input('(gracz2) Zgadnij liczbe od 1 do 20: '))
+
+odleglosc1 = fabs(losowa - gracz1)
+odleglosc2 = fabs(losowa - gracz2)
+
+print('Wylosowana liczba wynosi: ' + str(losowa))
+
+if odleglosc1 < odleglosc2:
+    print('Wygral gracznr 1')
+elif odleglosc1 > odleglosc2:
+    print('WYgral gracz nr 2')
+else:
+    print('Remis')
+
+
+
+
