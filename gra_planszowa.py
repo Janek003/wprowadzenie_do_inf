c2474d73ba467600fc9c5e12144fbf04c352a110
Janek003 <jan.porebski.lublin@gmail.com>
2024-10-24 17:57:11 +0200

    end_of_session

--- gra_planszowa.py
+++ gra_planszowa.py
@@ -8,14 +8,17 @@ pozycja_p1 = 0
 pozycja_p2 = 0
 
 while not game_finished:
+    wylosowana = random.randint(1, 4)
+    print(wylosowana)
 
-    pozycja_p1 += random.randint(1, 4)
+    pozycja_p1 += wylosowana
 
     if pozycja_p1 >= 19:
-
         game_finished = True
+
+    
 
 test = "01"
 
-print(test[2])
+print(test[0])
 
