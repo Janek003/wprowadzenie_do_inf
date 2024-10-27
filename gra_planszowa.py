e717e254348faa6f7071455dd75bcccc0d6e103f
Jan Porębski <120678651+Janek003@users.noreply.github.com>
2024-10-27 13:02:35 +0100

    Update gra_planszowa.py

--- gra_planszowa.py
+++ gra_planszowa.py
@@ -1,5 +1,5 @@
 import random
-pola = "01234567890123456789"
+
 oznaczenie_pola = "00a000000b000e000c0d"
 
 game_finished = False
@@ -7,18 +7,55 @@ game_finished = False
 pozycja_p1 = 0
 pozycja_p2 = 0
 
+skip = 0
+trap = False
+
 while not game_finished:
-    wylosowana = random.randint(1, 4)
-    print(wylosowana)
-
-    pozycja_p1 += wylosowana
-
-    if pozycja_p1 >= 19:
-        game_finished = True
-
     
+    if skip <= 0 and not trap:
+        
+        wylosowana = random.randint(1, 4)
+        input('Click any buton to roll the dice...')
+        print(f'You rolled: {wylosowana}.')
 
-test = "01"
+        pozycja_p1 += wylosowana
 
-print(test[0])
+        print(f'You ended up on field: {pozycja_p1}.')
 
+        if oznaczenie_pola[pozycja_p1] == "a":
+            print('You are standing on superfield(A) ->  You found a portal, you are teleporting 5 fields forward')
+            pozycja_p1 += 5
+            print(f'You ended up on field: {pozycja_p1}.')
+
+        
+        if oznaczenie_pola[pozycja_p1] == "b":
+            skip = random.randint(1,3)
+            print(f'You are standing on superfield(B) -> Your spaceship is broken, fixing it will take {skip} rounds')
+
+        if oznaczenie_pola[pozycja_p1] == "c":
+            print('You are standing on superfield(C) -> You remember you have forgotten something from base camp, you go back to start...')
+            pozycja_p1 = 0
+
+        if oznaczenie_pola[pozycja_p1] == "e":
+            print('You are standing on superfield(E) -> You get cought up in a trap, to get out of it in the next round, you will have to roll 4, good luck...')
+            trap = True
+
+        if pozycja_p1 >= 19:
+            print('You reached the end, you won!')
+            game_finished = True
+
+    elif skip > 0:
+        skip += -1
+        print(f'You are still fixing your spaceship, you have {skip} rounds left')
+
+
+    elif trap:
+        wylosowana = random.randint(1, 4)
+        print('You are still in a trap, to free yourself - you must roll 4.')
+        input('Click any buton to roll the dice...')
+        print(f'You rolled: {wylosowana}.')
+        if wylosowana == 4:
+            trap = False
+            pozycja_p1 += wylosowana
+
+input("Click any button to exit...")
