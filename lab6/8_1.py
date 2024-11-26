ec92863b400372075984788e8404d724f021eba9
Jan Porębski <120678651+Janek003@users.noreply.github.com>
2024-11-26 20:30:53 +0100

    Update 8_1.py

--- lab6/8_1.py
+++ lab6/8_1.py
@@ -1,34 +1,35 @@
+global ilosc_permutacji
 ilosc_permutacji = 0
 
 string = input("Lancuch znakow: ") #[abcsde]
 string_list = [i for i in string]
+string_unikalne = list(set(string_list))
 string_element_list = []
 
 for i in string_list:
     if i not in string_element_list: string_element_list.append(i)
 
-template_curr_list = ['' for i in range(len(string))]
+template_curr_list = ['' for _ in range(len(string_unikalne))]
 curr_str = template_curr_list
 
 
-def permutuj(n, curr_str, ilosc_permutacji):
-    #if n > (len(string) - 1):
-        #print(curr_str)
-        #curr_str[n] = ''
-        #ilosc_permutacji += 1
-        #return
-    if n < len(string):
-        for i in string_list:
-            ile_elementow = string_list.count(i)
-            print(f'\ncurr_str: {curr_str}\nn: {n}\ni: {i}, w stringu curr: {curr_str.count(i)}\ni: {i}, w stringu orginalnym: {ile_elementow}')
-            if curr_str.count(i) < ile_elementow:
-                print(f'\nprzypisujemy {i} do pozycji {n}')
-                curr_str[n] = i
-                permutuj(n+1, curr_str, ilosc_permutacji)
-        
-        curr_str[n] = ''
+def permutuj(n, curr_str):
+    if n >= len(string_unikalne):
+        print(curr_str)
 
-    return
+        global ilosc_permutacji
+        ilosc_permutacji += 1
 
-print(f"{string} \n{curr_str}")
-permutuj(0, curr_str, ilosc_permutacji)
+        return
+    
+    for i in string_unikalne:
+        #print(f'\ncurr_str: {curr_str}\nn: {n}\ni: {i}, w stringu curr: {curr_str.count(i)}\ni: {i}, w stringu orginalnym: {ile_elementow}')
+        if not i in curr_str:
+            #print(f'\nprzypisujemy {i} do pozycji {n}')
+            curr_str[n] = i
+            permutuj(n+1, curr_str)
+    
+    curr_str[n] = ''
+
+permutuj(0, curr_str)
+print(ilosc_permutacji)
