a3c0d695cc763e342dad03ea952bb6886bf5d39a
Jan Porębski <120678651+Janek003@users.noreply.github.com>
2024-11-27 18:42:26 +0100

    Update 8_2.py

--- lab6/8_2.py
+++ lab6/8_2.py
@@ -1,42 +1,37 @@
-n = int(input("n -> liczba dyskow, n = "))
+from copy import deepcopy
+
+# n = int(input("n -> liczba dyskow, n = "))
+n = 4
 
 min_moves = sum(2**i for i in range(n))
 print(min_moves)
 
-bad_moves = [[] for _ in range(min_moves)]
-print(bad_moves)
-
-poles = [[] for _ in range(3)]
-for i in range(n-1, -1, -1): poles[0].append(i)
-print(poles)
+poles_init = [[] for _ in range(3)]
+for i in range(n-1, -1, -1): poles_init[0].append(i)
+print(poles_init)
 
 which_to_move = [0 for _ in range(min_moves)]
 for a in range(n):
     for i in range(2**a - 1, min_moves - (2**a - 1), 2**(a+1)): which_to_move[i] = a
 print(which_to_move)
+print('---------------')
 
-def smallestOnPoleNr(pole_nr):
-    if poles[pole_nr] == []: return -1
+def smallestOnPoleNr(pole_nr, poles):
+    if poles[pole_nr] == []: return n
     return min(poles[pole_nr])
 
-def whereCanDiscGoNow(move_nr, current_disc_position):
+def canDiscGoThereNow(move_nr, pole_nr, poles):
     disc = which_to_move[move_nr]
-
-    if disc > smallestOnPoleNr(current_disc_position): return -1 #it cannot move
-
-    for pole_nr in range(3):
-        if not(pole_nr in bad_moves[move_nr] and disc > smallestOnPoleNr(pole_nr)): 
-            return pole_nr
-    
-    return -1
+    if disc >= smallestOnPoleNr(pole_nr, poles): return False 
+    return True
 
 def moveDiscFromTo(disc, pole_from, pole_to, poles):
-    updated_poles = poles
+    updated_poles = deepcopy(poles)
     updated_poles[pole_from].remove(disc)
     updated_poles[pole_to].append(disc)
     return updated_poles
 
-def whereIsDisc(disc):
+def whereIsDisc(disc, poles):
     for i in range(3):
         if disc in poles[i]: return i
 
@@ -45,23 +40,25 @@ def findMove(move_nr, poles):
         if poles[2] == [i for i in range(n-1, -1, -1)]:
             print(f"finished: {poles}")
             return True
-        else: return False
+        else:
+            return False
 
     disc = which_to_move[move_nr]
-    curr_disc_position = whereIsDisc(disc)
+    curr_disc_position = whereIsDisc(disc, poles)
+    if disc > smallestOnPoleNr(curr_disc_position, poles): 
+        return False 
 
-    pole_aviable = whereCanDiscGoNow(move_nr, curr_disc_position)
-    print(pole_aviable)
+    for pole_nr in range(3):
+        if canDiscGoThereNow(move_nr, pole_nr, poles):
+            updated_copy_of_poles = moveDiscFromTo(disc, curr_disc_position, pole_nr, poles)
+            findMove(move_nr+1, deepcopy(updated_copy_of_poles))
+    return False        
 
-    if pole_aviable == -1: return False
-    else:
-        updated_poles = moveDiscFromTo(disc, curr_disc_position, pole_aviable, poles)
-        print(updated_poles)
-        if not findMove(move_nr+1, updated_poles):
-            bad_moves[move_nr].append(pole_aviable)
-
-findMove(0, poles)
-# print()
-# poles[0][3] = ''
-# print(poles)
-# print(smallestOnPoleNr(0))
+findMove(0, deepcopy(poles_init))
+# najwiekszym problemem bylo dodawanie ruchow do bad_moves
+# nie powinno istniec cos takiego jak bad moves
+# jesli jest on zly to sam algorytm wycofa sie z tej drogi zwracajac falsz dla dalszych krokow i przechodzac do nastepnej opcji w petli for
+# uzywajac bad moves uniemozliwialem algorytmowi wykonanie ponownie tego samego ruchu dla kompletnie innego srodowiska
+# ogolnie w rekursji nie mozna uzywac tej techniki, bo sama rekursja do tego slozy
+# aviable moves nie maja myc najlepszymi ani prowadzacymi do sukcesu tylko rzeczywiscie mozliwymi
+# to petla for odrzuca zle elementy, ta petla for przechodzi tylko raz, wiec zaden element nie bedzie probowany w nieskaczonosc
