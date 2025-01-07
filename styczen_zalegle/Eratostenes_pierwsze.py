eda912c24824d1ada47dd2473c6e8f7fac9cb976
Jan Porębski <120678651+Janek003@users.noreply.github.com>
2025-01-07 10:57:16 +0100

    a

---
+++ styczen_zalegle/Eratostenes_pierwsze.py
@@ -0,0 +1,22 @@
+from math import sqrt
+
+n = int(input("koniec zakresu poszukiwania: "))
+
+tab = [True for i in range(n-1)] # True == pierwsza
+
+koniec_poszukiwan = int(sqrt(n))
+print(koniec_poszukiwan)
+
+for i in range(koniec_poszukiwan):
+    if tab[i] == False: continue
+    
+    wartosc = i + 2
+    for j in range(i+1, n-1):
+        if (j + 2) % wartosc == 0:
+            tab[j] = False
+
+print(tab)
+
+    
+    
+
