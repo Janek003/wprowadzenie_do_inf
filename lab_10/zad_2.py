067abcb08863d007b9d64da468efbbaaf0a9aac9
Jan Porębski <120678651+Janek003@users.noreply.github.com>
2025-01-15 13:57:04 +0100

    a

--- lab_10/zad_2.py
+++ lab_10/zad_2.py
@@ -3,6 +3,11 @@ import random
 import time
 import copy
 import msvcrt
+import winsound
+import threading
+
+def beepNoBlock(frequency, duration):
+    threading.Thread( target=winsound.Beep, args=(frequency, duration) ).start()
 
 def wylosujJablko():
     head_cords_ij = player_cords_ij[-1]
@@ -22,10 +27,13 @@ def wylosujJablko():
         print("jablko nie moglo zostac stworzone, wygrales?")
         return
 
-    a = random.randint( 0, len(free_space) )
+    a = random.randint( 0, len(free_space) - 1 )
     return free_space[a]
     
 def wyswietlPlansze():
+
+    print(f"ZYCIA: {zycia}\tPUNKTY: {punkty}")
+
     view_plansza = copy.deepcopy(plansza)
     
     i, j = jablko_cords_ij
@@ -55,25 +63,49 @@ def poruszWeza():
         new_j = head_j + 1
 
     player_cords_ij.append( [new_i, new_j] )
-    player_cords_ij.pop(0)
+    if len(player_cords_ij) > dlugosc_weza:
+        player_cords_ij.pop(0)
 
 def zwrocKierunek(kier):
-    if msvcrt.kbhit() == True:
-        klawisz = msvcrt.getwch()
+
+
+    if msvcrt.kbhit():
+        while msvcrt.kbhit():
+            klawisz = msvcrt.getwch()
+
         if klawisz == 'w': return 'N' 
         if klawisz == 'd': return 'E' 
         if klawisz == 's': return 'S' 
         if klawisz == 'a': return 'W'
     return kier
 
+def czyTraciZycie():
+    for cord in player_cords_ij:
+        if player_cords_ij.count(cord) > 1:
+            os.system("cls")
+            wyswietlPlansze()
+            return True
+
+    i, j = player_cords_ij[-1]
+    if plansza[i][j] == '#': return True
+
+    return False
+
 
 global gra_trwa
 gra_trwa = True
 
-#a = input("relative path do planszy: ")
-a = "plansza.txt"
+a = input("relative path do planszy: ")
+#a = "plansza.txt"
 with open(f"{os.getcwd()}\\{a}") as f:
     plansza = [[element for element in line.split(" ")] for line in f.read().split("\n")]
+
+zycia = 3
+dlugosc_weza = 1
+punkty = 0
+
+sleep_time_start = 0.5
+sleep_time = sleep_time_start
 
 width = len(plansza[0])
 height = len(plansza)
@@ -82,18 +114,56 @@ player_cords_ij = [ [0, 0] ]
 player_cords_ij = [ wylosujJablko() ]
 
 #wybor pierwszego kierunku, gra nie zaczyna sie dopoki nie zostanie wybrany
-wyswietlPlansze()
 jablko_cords_ij = wylosujJablko()
+
+os.system("cls")
+wyswietlPlansze()
 
 kierunek = ''
 while kierunek == '':
     kierunek = zwrocKierunek(kierunek)
 
-while gra_trwa == True:
-    kierunek = zwrocKierunek(kierunek)
 
-    time.sleep(1.5)
+
+while gra_trwa == True:
+
+    kierunek = zwrocKierunek(kierunek)
+    poruszWeza()
+
+    if czyTraciZycie():
+        winsound.Beep(500, 300)
+        time.sleep(0.1)
+        winsound.Beep(300, 400)
+        zycia -= 1
+
+        dlugosc_weza = 1
+        sleep_time = sleep_time_start
+
+        player_cords_ij = [ [0, 0] ]
+        player_cords_ij = [ wylosujJablko() ]
+
+        os.system("cls")
+        wyswietlPlansze()
+
+        if zycia == 0:
+            input("\nKONIEC GRY...")
+            break
+
+
+        
+        kierunek = ''
+        while kierunek == '':
+            kierunek = zwrocKierunek(kierunek)
+
+    if player_cords_ij[-1] == jablko_cords_ij:
+        sleep_time /= 1.03
+        beepNoBlock(700, 200)
+        dlugosc_weza += 1
+        punkty += 1
+        
+        jablko_cords_ij = wylosujJablko()
+        
     os.system("cls")
-    
-    jablko_cords_ij = wylosujJablko()
     wyswietlPlansze()
+
+    time.sleep(sleep_time)
